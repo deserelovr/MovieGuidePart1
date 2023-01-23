62c37bf209c1a79243285ab85c32a6e96cb3bcbd
jasmine.bumbray@my.strayer.edu <jasmine.bumbray@my.strayer.edu>
2023-01-23 08:59:10 -0800

    added def statement

--- MovieGuidePart1.py
+++ MovieGuidePart1.py
@@ -13,3 +13,23 @@ def Display_movie():
 def List(movie_list):
     for l, movie in enumerate(movie_list, start=1):
         print(f"{l}. {movies}")
+    
+
+def Add(movie_list):
+    movie = input("Enter Movie name: ")
+    movie_list.append(movie)
+    print(f'{movie} has been added.\n')
+
+def Remove(movie_list):
+    number = int(input("Number: "))
+    if number < 1 or number > len(movie_list):
+        print("Invalid movie number.\n")
+    else:
+        movie = movie_list.pop(number-1)
+        print(f"{movie} was deleted.\n")
+
+
+def main():
+   movie_list["Nobody," "Mean Girls," "Boyz N the Hood"]
+
+   display_menu()
