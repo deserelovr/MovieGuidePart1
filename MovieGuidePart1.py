f0bc56489dac0c39c816a92391fe667061428bfe
jasmine.bumbray@my.strayer.edu <jasmine.bumbray@my.strayer.edu>
2023-01-23 08:04:03 -0800

    added the first section of code

--- MovieGuidePart1.py
+++ MovieGuidePart1.py
@@ -1,3 +1,15 @@
 #Jasmine BUmbray
 #CIS261
 #MovieGuidePart1
+
+def Display_movie():
+    print("Movie List\n")
+    print("Command Menu")
+    print("List - Show all movies")
+    print("Add - Add a new movie")
+    print("Remove - Delete a movie from list")
+    print("Exit - leave the list\n")
+
+def List(movie_list):
+    for l, movie in enumerate(movie_list, start=1):
+        print(f"{l}. {movies}")
